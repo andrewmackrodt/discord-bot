e730b85c3ae84f4492bdfc2a64f39a2f2c8befd6
Andrew Mackrodt <andrewmackrodt@gmail.com>
2023-08-19 22:06:20 +0100

    Reduce docker image size

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,31 @@
 ################################################################################
-# Stage 1 : Create the dist bundle
+# Builder
 ################################################################################
-FROM node:18-alpine AS dist
+FROM node:18-alpine AS builder
+ENV NPM_CONFIG_UPDATE_NOTIFIER=false
+RUN npm i -g json
+COPY package.json package-lock.json /opt/app/
 WORKDIR /opt/app
-COPY package*.json /opt/app/
-RUN npm ci --silent --no-progress
+RUN npm i --silent --no-progress \
+ && mkdir out \
+ && cat package.json \
+      | json -q -e 'this.scripts = { "start": "node index.js" }; delete this.devDependencies' \
+      | tee out/package.json >/dev/null \
+ && npm i --silent --no-progress --omit=dev --package-lock-only --prefix=out/ \
+ && cd out \
+ && npm i --silent --no-progress --omit=dev
 COPY . /opt/app/
-RUN npm run build --silent
+RUN npm run build:compile --silent
 
 ################################################################################
-# Stage 2 : Install runtime dependencies and copy the dist bundle
+# Target
 ################################################################################
 FROM node:18-alpine
 RUN apk add --no-cache font-noto graphicsmagick imagemagick tini
-COPY --from=dist /opt/app/dist/ /opt/app
+COPY --from=builder /opt/app/out/ /opt/app/
 WORKDIR /opt/app
 RUN mkdir -p /config \
  && ln -s /config /opt/app/config
 VOLUME /config
 ENV NODE_ENV=production
-ENTRYPOINT ["/sbin/tini", "--", "node", "index"]
+ENTRYPOINT ["/sbin/tini", "--", "node", "index.js"]
---
+++ build.ts
@@ -0,0 +1,50 @@
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs'
+import { copySync } from 'fs-extra'
+import { glob } from 'glob'
+import { exec } from 'shelljs'
+
+const distPath = `${__dirname}/out`
+
+exec(['tsc',
+        '--sourcemap',
+        '--outDir',
+        `"${distPath}/"`,
+    ].join(' '),
+    {
+        fatal: true,
+    })
+
+glob.sync(`${__dirname}/src/plugins/*/assets`).map(filepath => {
+    const dest = `${distPath}/` + filepath.split('/').slice(-4).join('/')
+    mkdirSync(dest)
+    copySync(filepath, dest)
+})
+
+copySync(`${__dirname}/ormconfig.json`, `${distPath}/ormconfig.json`)
+
+const packageJsonFilepath = `${distPath}/package.json`
+const packageLockFilepath = `${distPath}/package-lock.json`
+let packageLockExists = existsSync(packageLockFilepath)
+
+// don't regenerate package.json and package-lock.json if they already exist
+// e.g. during docker build
+if ( ! existsSync(packageJsonFilepath)) {
+    const packageJson = JSON.parse(readFileSync(`${__dirname}/package.json`).toString('utf8'))
+    packageJson.scripts = { start: 'node index.js' }
+    delete packageJson.devDependencies
+    writeFileSync(packageJsonFilepath, JSON.stringify(packageJson, null, 2))
+    packageLockExists = false
+}
+
+if ( ! packageLockExists) {
+    exec(['npm', 'i',
+            '--silent',
+            '--no-progress',
+            '--omit=dev',
+            '--package-lock-only',
+            `--prefix="${distPath}/"`,
+        ].join(' '),
+        {
+            fatal: true,
+        })
+}
