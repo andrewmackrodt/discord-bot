0b5d7e62aa369f71bdd77d08e6f7b4fd12d9d0bc
Andrew Mackrodt <andrewmackrodt@gmail.com>
2023-08-29 22:52:41 +0100

    Add a roll dice plugin

---
+++ src/plugins/roll/index.test.ts
@@ -0,0 +1,48 @@
+import type { Message } from 'discord.js'
+import { mock, when, It } from 'strong-mock'
+import RollPlugin from './index'
+
+describe('replyRoll()', () => {
+    async function parseRollResult(input?: string) {
+        const mockMessage = mock<Message>()
+        const matcher = It.willCapture<string>()
+        when(() => mockMessage.reply(matcher)).thenResolve(mockMessage)
+        await new RollPlugin().replyRoll(mockMessage, input)
+        const [total, rolls] = matcher.value!.replaceAll(/[^0-9=+]/g, '').split('=')
+        return { total: parseInt(total), rolls: rolls.split('+').map(s => parseInt(s)) }
+    }
+
+    afterEach(() => {
+        jest.spyOn(Math, 'random').mockRestore()
+    })
+
+    it('defaults to 1d6 when no input', async () => {
+        jest.spyOn(Math, 'random').mockReturnValue(0.99999)
+        const { total, rolls } = await parseRollResult()
+        expect(rolls).toHaveLength(1)
+        expect(total).toEqual(6)
+        expect(total).toEqual(rolls.reduce((res, roll) => res + roll, 0))
+    })
+
+    it('rolls 1 when random is zero', async () => {
+        jest.spyOn(Math, 'random').mockReturnValue(0)
+        const { total, rolls } = await parseRollResult()
+        expect(rolls).toHaveLength(1)
+        expect(total).toEqual(1)
+        expect(total).toEqual(rolls.reduce((res, roll) => res + roll, 0))
+    })
+
+    it('sums result of input 1d10', async () => {
+        jest.spyOn(Math, 'random').mockReturnValue(0.99999)
+        const { total, rolls } = await parseRollResult('1d10')
+        expect(rolls).toHaveLength(1)
+        expect(total).toEqual(10)
+        expect(total).toEqual(rolls.reduce((res, roll) => res + roll, 0))
+    })
+
+    it('sums result of input 2d20', async () => {
+        const { total, rolls } = await parseRollResult('2d20')
+        expect(rolls).toHaveLength(2)
+        expect(total).toEqual(rolls.reduce((res, roll) => res + roll, 0))
+    })
+})
---
+++ src/plugins/roll/index.ts
@@ -0,0 +1,41 @@
+import { Message } from 'discord.js'
+import { command, CommandUsageError } from '../../utils/command'
+
+export default class RollPlugin {
+    @command('roll', {
+        emoji: '🎲',
+        title: 'Roll Dice',
+        description: 'Roll some dice.',
+        args: {
+            input: { example: '2d10' },
+        },
+    })
+    public async replyRoll(message: Message, input?: string): Promise<any> {
+        let count: number, sides: number
+        if (input) {
+            const args = input.split('d').map(s => parseInt(s))
+            if (args.length !== 2 || args.find(n => isNaN(n))) {
+                throw new CommandUsageError('roll', 'use format "2d10" for 2 dice with 10 sides')
+            }
+            [count, sides] = args
+            if (10 < count || count < 1) {
+                throw new CommandUsageError('roll', 'count must be between 1 and 10')
+            }
+            if (20 < sides || sides < 4) {
+                throw new CommandUsageError('roll', 'sides must be between 4 and 20')
+            }
+        } else {
+            count = 1
+            sides = 6
+        }
+        const rolls: number[] = []
+        let total = 0
+        for (let i = 0; i < count; i++) {
+            const roll = 1 + Math.floor(Math.random() * sides)
+            total += roll
+            rolls.push(roll)
+        }
+        const text = `**${total}** = (` + rolls.join(' + ') + ')'
+        return message.reply(`🎲 ${text}`)
+    }
+}
